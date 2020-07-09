27c7d861d5b64fa3e4847252b8b00d3e3feb2a0b
Jakub Znamenacek <jakub.znamenacek@siemens.com>
2020-07-09 20:08:14 +0200

    Additional things

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -18,6 +18,14 @@ public class Main {
         List<EndpointDescription> endpoints = DiscoveryClient.getEndpoints("opc.tcp://192.168.56.10:4840").get();
         System.out.println("Number of endpoints = " + endpoints.size());
         endpoints.stream().forEach(System.out::println);
+
+        //Endpoints parameters
+        EndpointDescription firstEndpoint = endpoints.get(0);
+        System.out.println("EndpoitUrl: "+firstEndpoint.getEndpointUrl());
+        System.out.println("ServerCertificate: "+firstEndpoint.getServerCertificate()); //For security
+        System.out.println("SecurityPolicyUri: "+firstEndpoint.getSecurityPolicyUri()); //For security
+        System.out.println("SecurityMode: "+firstEndpoint.getSecurityMode()); //For security
+        System.out.println("UserIdentityTokens: "+firstEndpoint.getUserIdentityTokens());
         System.out.println();
 
         //select endpoint
@@ -33,6 +41,8 @@ public class Main {
         //Data value
         System.out.println(client.readValue(0, TimestampsToReturn.Both,new NodeId(3,"SerialNumber")).get()); //Read directly
         System.out.println();
+        //Program is running and this is done when the value is ready
+        client.readValue(0, TimestampsToReturn.Both,new NodeId(3,"SerialNumber")).thenAccept(System.out::println);
 
         //Browse nodes
         System.out.println("BROWSE NODES:");
